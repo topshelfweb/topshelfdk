54f9e59a14c06a30c5f65310729dad1263b51cc0
Brian Emilius <brian@brianemilius.com>
2020-07-01 12:56:06 +0200

    add responsive layout and logo to site-header

--- src/components/site-header/index.js
+++ src/components/site-header/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import Navbar from "../navbar";
 import { Link } from "gatsby";
 import "./header.css";
+import logo from "../../img/logo.svg";
 
 function SiteHeader() {
 	return (
 		<header className="siteHeader wrapper">
-			<Link to="/" class="siteHeader__brand">firmalogo</Link>
+			<Link to="/" className="siteHeader__brand">
+				<img src={logo} height="48" alt="topshelf logo" className="siteBrand__logo" />
+			</Link>
 			<Navbar />
 		</header>
 	)
