afcce99eef435f4167bf77c0ae92a8aaa9838f5e
Brian Emilius <brian@brianemilius.com>
2020-07-02 12:31:21 +0200

    add Card component

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import "./services.css";
+import Card from "../card";
 
 function Services({services}) {
 	return (
 		<section className="flex serviceContainer">
 			{services.map(service => {
-				return (
-					<article className="serviceCard" key={ service.title }>
-						<img src={ service.image.publicURL ? service.image.publicURL : service.image } className="serviceCard__image" alt="" />
-						<h1>{ service.title }</h1>
-				<p>{ service.content }</p>
-					</article>
-				)
+				return <Card data={service} />
 			})}
 		</section>
 	)
