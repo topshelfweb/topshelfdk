b44f51edb35fce0a4f074c053e471a10a17f2519
Brian Emilius <brian@brianemilius.com>
2026-01-23 12:25:55 +0100

    feat: add smooth scroll behavior to HTML layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="da">
+    <html lang="da" data-scroll-behavior="smooth">
       <body
         className={`${comfortaa.variable} antialiased`}
       >
