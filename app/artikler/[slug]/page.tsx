07f48d4dcf8b8458cb22b3d66cea2e7547fac8b3
Brian Emilius <brian@brianemilius.com>
2026-01-26 09:41:24 +0100

    add: related articles

--- app/artikler/[slug]/page.tsx
+++ app/artikler/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getAllPosts, getPostBySlug } from "@/lib/blog";
+import { BlogPost, getAllPosts, getPostBySlug, getPostsByTag } from "@/lib/blog";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import hljs from 'highlight.js';
@@ -6,6 +6,7 @@ import 'highlight.js/styles/default.css';
 import Container from "@/components/ui/container";
 import Link from "next/link";
 import { FaChevronRight } from "react-icons/fa";
+import ArticleCard from "@/components/ui/article-card";
 
 export async function generateStaticParams() {
 	const posts = await getAllPosts();
@@ -55,23 +56,50 @@ export async function generateMetadata({ params }: BlogPostPageProps): Promise<M
 export default async function BlogPage({ params }: BlogPostPageProps) {
 	const { slug } = await params;
 	const post = await getPostBySlug(slug);
-
+		
 	if (!post) {
 		return notFound();
 	}
 
+	const relatedPostsSet = new Set<BlogPost>();
+	for (const tag of post.tags) {
+		const postsWithTag = await getPostsByTag(tag);
+		for (const relatedPost of postsWithTag) {
+			if (relatedPost.slug !== post.slug) {
+				relatedPostsSet.add(relatedPost);
+			}
+			if (relatedPostsSet.size >= 3) {
+				break;
+			}
+		}
+		if (relatedPostsSet.size >= 3) {
+			break;
+		}
+	}
+	const relatedPosts = Array.from(relatedPostsSet).slice(0, 3);
+	
 	return (
 		<>
 			<Container className="py-8">
 				<p><Link href="/">Hjem</Link> <FaChevronRight className="inline align-middle mb-1" /> <Link href="/artikler">Artikler</Link> <FaChevronRight className="inline align-middle mb-1" /> {post.title}</p>
-				<article id="blogPost">
-					<header>
-						<h1 className="font-heading text-3xl">{post.title}</h1>
-						<p>Af {post.author}, <time dateTime={post.date}>{new Date(post.date).toLocaleDateString("da-DK", { day: "numeric", weekday: "long", month: "long", year: "numeric" })}</time></p>
-						<p>{post.readingTime.words} ord, ca. {Math.round(post.readingTime.minutes)} minutter</p>
-					</header>
-					<div dangerouslySetInnerHTML={{ __html: post.content }} className="pt-8" />
-				</article>
+				<div className="xl:flex xl:gap-16">
+					<article id="blogPost" className="xl:flex-3">
+						<header>
+							<h1 className="font-heading text-3xl">{post.title}</h1>
+							<p>Af {post.author}, <time dateTime={post.date}>{new Date(post.date).toLocaleDateString("da-DK", { day: "numeric", weekday: "long", month: "long", year: "numeric" })}</time></p>
+							<p>{post.readingTime.words} ord, ca. {Math.round(post.readingTime.minutes)} minutter</p>
+						</header>
+						<div dangerouslySetInnerHTML={{ __html: post.content }} className="pt-8" />
+					</article>
+					<aside className="xl:flex-1">
+						<h2 className="font-heading text-2xl mt-12 mb-4">Relaterede artikler</h2>
+						<ul className="flex flex-col gap-4">
+							{relatedPosts.map((relatedPost: BlogPost) => (
+								<ArticleCard key={relatedPost.slug} article={relatedPost} />
+							))}
+						</ul>
+					</aside>
+				</div>
 				<hr />
 				<p><Link href="#top">Tilbage til toppen</Link></p>
 				<script>hljs.highlightAll()</script>
