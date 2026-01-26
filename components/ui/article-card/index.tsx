d57b2a5764b5759100bff45f7464a371a07fe4ef
Brian Emilius <be@rts.dk>
2026-01-26 20:08:50 +0100

    fix format of date string in article card

--- components/ui/article-card/index.tsx
+++ components/ui/article-card/index.tsx
@@ -2,11 +2,14 @@ import { BlogPost } from "@/lib/blog";
 import Link from "next/link";
 
 export default function ArticleCard({ article }: { article: BlogPost }) {
+	let formatedDate = new Date(article.date).toLocaleDateString("da-DK", { day: "numeric", weekday: "long", month: "long", year: "numeric" });
+	formatedDate = formatedDate.charAt(0).toUpperCase() + formatedDate.slice(1);
+
 	return (
 		<Link href={`/artikler/${article.slug}`}>
-			<div className="dark:bg-iceblue bg-snowwhite p-8 rounded-md min-h-full text-nordicink">
+			<div className="dark:bg-iceblue bg-snowwhite p-8 xl:p-4 rounded-md min-h-full text-nordicink">
 				<h2 dangerouslySetInnerHTML={{ __html: article.title }} className="text-3xl font-heading" />
-				<time dateTime={article.date} className="capitalize text-sm">{new Date(article.date).toLocaleDateString("da-DK", { day: "numeric", weekday: "long", month: "long", year: "numeric" })}</time>
+				<time dateTime={article.date} className="text-sm">{formatedDate}</time>
 				<p dangerouslySetInnerHTML={{ __html: article.excerpt }} />
 			</div>
 		</Link>
