217153402cce38d229479da23e0d012eab0038b3
Brian Emilius <brian@brianemilius.com>
2026-02-02 12:55:52 +0100

    fix: restructure atom author tag in RSS feed for proper XML formatting

--- app/rss.xml/route.ts
+++ app/rss.xml/route.ts
@@ -5,7 +5,9 @@ export async function GET(request: Request) {
 
 	const postsInRSSFormat = posts.map(post => (`<item>
 			<title>${post.title}</title>
-			<atom:author>${post.author}</atom:author>
+			<atom:author>
+				<atom:name>${post.author}</atom:name>
+			</atom:author>
 			<description>${post.excerpt}</description>
 			<link>https://www.topshelf.dk/artikler/${post.slug}</link>
 			<guid>https://www.topshelf.dk/artikler/${post.slug}</guid>
