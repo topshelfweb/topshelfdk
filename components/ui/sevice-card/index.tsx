6c5546a65f0502e9b9a319ea8373b02643906ee3
Brian Emilius <brian@brianemilius.com>
2026-02-19 13:56:53 +0100

    update headings on cards

--- components/ui/sevice-card/index.tsx
+++ components/ui/sevice-card/index.tsx
@@ -19,7 +19,7 @@ export default function ServiceCard({ service }: { service: Service }) {
 		<li className="flex-[1_1_0]">
 			{/* <Link href={service.link}> */}
 			<article className="p-8 rounded-md min-h-full bg-snowwhite dark:bg-fjordblue drop-shadow-xl dark:drop-shadow-none">
-				<h2 dangerouslySetInnerHTML={{ __html: service.title }} className="text-3xl font-heading" />
+				<h3 dangerouslySetInnerHTML={{ __html: service.title }} className="text-3xl font-heading" />
 				<Image src={service.image.src} width={service.image.width} height={service.image.height} alt={service.image.alt} priority />
 				<p dangerouslySetInnerHTML={{ __html: service.text }} />
 			</article>
