a261f1a0916baa7b1ebc2451200896625f958c78
Brian Emilius <hello@brianemilius.com>
2026-02-25 20:32:40 +0100

    update: newsletter signup form design

--- components/forms/newsletter-signup/index.tsx
+++ components/forms/newsletter-signup/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useActionState, useEffect, useRef } from "react";
-import NewsletterAction, { FormState } from "./newsletter-action";
+import NewsletterAction from "./newsletter-action";
 
 export default function NewsletterSignupForm() {
 	const [formState, formAction, isPending] = useActionState(NewsletterAction, { data: { email: "" } });
@@ -14,14 +14,23 @@ export default function NewsletterSignupForm() {
 	return (
 		<form className="text-center" action={formAction}>
 			<div className="mb-4 flex flex-col gap-4 lg:flex-row items-center">
-				<label>
-					<span>E-mail&nbsp;</span>
-					<input ref={inputRef} type="email" name="email" autoComplete="email" defaultValue={formState.data.email} className={`bg-snowwhite px-1 py-1 text-nordicink ${formState.errors?.length && "border border-red-600"}`} placeholder="din@email.dk" required />
+				<label className="relative">
+					<span className="text-sm absolute text-nordicink ml-2 mt-1">E-mail</span>
+					<input
+						ref={inputRef}
+						type="email"
+						name="email"
+						autoComplete="email"
+						defaultValue={formState.data.email}
+						className={`bg-snowwhite px-2 pt-6 pb-2 w-64 text-nordicink ${formState.errors?.length ? "border-2 border-red-600": ""}`}
+						placeholder="din@email.dk"
+						required
+					/>
 				</label>
 				<button
 					disabled={isPending}
 					type="submit"
-					className="bg-fjordblue disabled:bg-nordicink text-iceblue border-none relative py-2 px-4 font-bold after:absolute after:border-2 after:border-iceblue after:top-0 after:right-0 after:bottom-0 after:left-0 after:m-[.2rem]">
+					className="bg-fjordblue disabled:bg-nordicink text-iceblue border-none relative py-4 px-4 font-bold after:absolute after:border-2 after:border-iceblue after:top-0 after:right-0 after:bottom-0 after:left-0 after:m-[.2rem]">
 					Ja tak, send mig nyhedsbrevet
 				</button>
 			</div>
