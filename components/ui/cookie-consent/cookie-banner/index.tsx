e8e35653983fa9e59688369309ee395ab1d54bc6
Brian Emilius <hello@brianemilius.com>
2026-02-28 19:52:11 +0100

    update colors for cookie consent banner to match the design

--- components/ui/cookie-consent/cookie-banner/index.tsx
+++ components/ui/cookie-consent/cookie-banner/index.tsx
@@ -46,20 +46,20 @@ export function CookieBanner({ showBanner }: CookieBannerProps) {
 
 	return (
 		<motion.div initial={{ transform: "translateY(200px)" }} animate={{ transform: "translateY(0)" }} className="fixed bottom-0 left-0 w-full min-h-32">
-			<div className="bg-fjordblue h-12">
+			<div className="bg-huskyfur h-12">
 				<Container>
 					<h2 className="leading-12">Cookies</h2>
 				</Container>
 			</div>
-			<div className="bg-nordicink text-iceblue py-8">
+			<div className="bg-snowwhite text-nordicink py-8">
 				<Container>
 					<p>
 						Vi bruger cookies til funktionalitet og analyse. Du kan acceptere alle, afvise alle eller justere dine præferencer individuelt.
 					</p>
 					<div className="flex gap-8">
 						<button onClick={handleAcceptAll} disabled={isProcessing} className="bg-pinegreen text-iceblue border-none relative py-2 px-4 font-bold after:absolute after:border-2 after:border-iceblue after:top-0 after:right-0 after:bottom-0 after:left-0 after:m-[.2rem]">Accepter alle</button>
-						<button onClick={handleRejectAll} disabled={isProcessing} className="text-iceblue border-none relative py-2 px-4 after:absolute after:border after:border-iceblue after:top-0 after:right-0 after:bottom-0 after:left-0 after:m-[.2rem]">Afvis alle</button>
-						<button onClick={() => setShowSettings(true)} disabled={isProcessing} className="text-iceblue border-none relative py-2 px-4 after:absolute after:border after:border-iceblue after:top-0 after:right-0 after:bottom-0 after:left-0 after:m-[.2rem]">Cookie-indstillinger</button>
+						<button onClick={handleRejectAll} disabled={isProcessing} className="bg-huskyfur text-nordicink border-none relative py-2 px-4 after:absolute after:border after:border-nordicink after:top-0 after:right-0 after:bottom-0 after:left-0 after:m-[.2rem]">Afvis alle</button>
+						<button onClick={() => setShowSettings(true)} disabled={isProcessing} className="bg-huskyfur text-nordicik border-none relative py-2 px-4 after:absolute after:border after:border-nordicink after:top-0 after:right-0 after:bottom-0 after:left-0 after:m-[.2rem]">Cookie-indstillinger</button>
 					</div>
 				</Container>
 			</div>
